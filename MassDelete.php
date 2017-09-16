533c771d73cbd65582ee8bf0423a7375806a52ce
Andy Martin <andy123@stanford.edu>
2017-09-15 17:08:45 -0700

    switched a lot of logic to class

--- MassDelete.php
+++ MassDelete.php
@@ -7,15 +7,170 @@ class MassDelete extends \ExternalModules\AbstractExternalModule
 	public $project_id;
 
 	public $records;
+	public $record_checkboxes = array();	// Array to hold checkboxes for display
 
-	public $arm;
+	public $errors = array();
+	public $notes = array();
+	public $my_rights = array();			// Array of user's userrights
+	public $group_id = null;				// The current user's group ID
 
+	public $arm_options = array();			// Array for select of arms/records
+	public $arm = null;						// Active arm number
+	public $arm_id = null;					// Arm ID
 
     public function __construct()
 	{
 		parent::__construct();
 	}
 
+	// TODO: What is the bast way to render a project-level header but be able to inject js and CSS...
+
+	// public function renderHeader() {
+	// 	// Render the viewer
+	// 	$html = new \HtmlPage();
+	//
+	// 	// Include cs file with file-modification-based timestamp (to aid with caching issues during development)
+	// 	$cs_file = "css/log_view.css";
+	// 	$cs_file_full = $cs_file . "?random=" . filemtime( __DIR__ . DS . $cs_file);
+	//
+	// 	$html->addStylesheet2( $this->getUrl($cs_file_full), "" );
+	// 	$html->PrintHeaderExt();
+	//
+	// 	// Include js file with file-modification-based timestamp (to aid with caching issues during development)
+	// 	$js_file = "js/log_view.js";
+	// 	$js_file_full = $js_file . "?random=" . filemtime( __DIR__ . DS . $js_file );
+	// 	$js_url = $this->getUrl($js_file_full);
+	// 	print "<script type='text/javascript' src='$js_url'></script>";
+	//
+	// 	}
+
+	public function validateUserRights($right = 'design') {
+		# Make sure user has permissions for project or is a super user
+		$these_rights = \REDCap::getUserRights(USERID);
+		$my_rights = $these_rights[USERID];
+		if (!$my_rights[$right] && !SUPER_USER) {
+			$this->errors[] = "Project Setup rights are required to access MASS DET plugin.";
+			$this->renderErrorPage();
+		}
+
+		# Make sure the user's rights have not expired for the project
+		if ($my_rights['expiration'] != "" && $my_rights['expiration'] < TODAY) {
+			$this->errors[] = 'Your user account has expired for this project.  Please contact the project admin.';
+			$this->renderErrorPage();
+		}
+		$this->my_rights = $my_rights;
+	}
+
+
+	public function determineRecords() {
+		// Check DAG
+		if ( !empty( \REDCap::getGroupNames() ) && !empty($this->my_rights['group_id']) ) {
+			$this->group_id = $this->my_rights['group_id'];
+			$this->notes['dag_filter'] = "Filtering records for your group: " . \REDCap::getGroupNames(FALSE, $this->group_id);
+		}
+
+		// Check ARM
+		global $Proj;
+		if ($Proj->longitudinal && $Proj->multiple_arms) {
+			// Get the current arm or default to 1
+			$arm = isset($_POST['arm']) ? $_POST['arm'] : 1;
+
+			// Verify the current arm is valid
+			if (!isset($Proj->events[$arm])) {
+				$notes[] = "Unable to find arm $arm in this project - setting the first arm as active";
+				unset($_POST['delete']);           // In case this is a delete post, prevent the delete from happening
+				$events = $Proj->events;
+				$arm = key($events);            // Take the frist arm number as the current
+			}
+			$this->arm = $arm;
+			$this->arm_id = $Proj->getArmIdFromArmNum($arm);
+
+			// Loop through all arms to build select box
+			$this->arm_options = array();
+			foreach ($Proj->events as $arm_num => $arm_detail) {
+				$arm_records = \Records::getRecordList($Proj->project_id, $this->group_id, false, false, $arm_num);
+				if ($arm == $arm_num) {
+					// Pull the current records
+					$this->records = $arm_records;
+					$selected = "selected='selected'";
+					$notes[] = "Showing records from arm $arm (" . $arm_detail['name'] . ")";
+				} else {
+					$selected = "";
+				}
+				$this->arm_options[] = "<option value='$arm_num' $selected> {$arm_detail['name']} - " .
+					count($arm_records) . " records</option>";
+			}
+		} else {
+			// Get all records
+			$this->records = \Records::getRecordList($Proj->project_id, $this->group_id);
+		}
+
+		// Obtain custom record label & secondary unique field labels for ALL records.
+		$extra_record_labels = \Records::getCustomRecordLabelsSecondaryFieldAllRecords($this->records, true, $this->arm_id);
+
+		// BUILD CHECKBOXES
+		$cbx_array = array();
+		$max_length = 0;
+		foreach ($this->records as $record) {
+			$label = $record .  ( empty($extra_record_labels[$record]) ? "" : " " . $extra_record_labels[$record]);
+			$max_length = max($max_length,strlen($label));
+
+			$cbx_array[] = "<input type='checkbox' name='records[]' value='$record'/> $label";
+		}
+
+		unset($extra_record_labels);
+
+		$max_length = $max_length + 3; // add space for checkbox
+
+		$this->max_length = $max_length;
+		$this->record_checkboxes = $cbx_array;
+
+	}
+
+	public function handlePost() {
+		if (isset($_POST['delete']) && $_POST['delete'] == 'true') {
+
+			// DELETE THE RECORD
+			$post_records = $_POST['records'];
+			$valid_records = array_intersect($post_records,$this->records);
+
+			if (count($valid_records) != count($post_records)) {
+				$this->errors[] = "Invalid records were requested for deletion.  Please try again.";
+				self::renderErrorPage();
+			} else {
+				// Continue to process
+				global $Proj;
+				foreach ($valid_records as $record) {
+					// print "<br>Deleting $record";
+					\Records::deleteRecord(
+						$record,
+						$Proj->table_pk,
+						$Proj->multiple_arms,
+						$Proj->project_id['randomization'],
+						$Proj->project['status'],
+						$Proj->project['require_change_reason'],
+						$this->arm_id,
+						" (" . $this->getModuleName() . ")"
+					);
+				}
+				$this->notes[] = "<b>Deleted " . count($valid_records) . " record" .
+					(count($valid_records > 1) ? "s" : "") .
+					"</b>";
+
+				// Refresh the record lists as they may have changed since the deletion
+				$this->determineRecords();
+			}
+		}
+	}
+
+	public function renderErrorPage($msg = '') {
+		if (!empty($msg)) print "<h3>ERROR:</h3>$msg";
+
+    	if (!empty($this->errors)) {
+			print "<div class='alert alert-danger'><ul><li>" . implode("</li><li>", $this->errors) . "</li></ul></div>";
+		}
+		exit();
+	}
 
 }
 
--- project_mass_delete.php
+++ project_mass_delete.php
@@ -1,191 +1,66 @@
 <?php
 
-$em = new \Stanford\MassDelete\MassDelete();
+$massDelete = new \Stanford\MassDelete\MassDelete();
+
 
 require_once \ExternalModules\ExternalModules::getProjectHeaderPath();
 
-?>
-<h1>This is the <?= $em->getModuleName() ?> project homepage!</h1>
-<?php
-
-
 // Page title
-renderPageTitle("<img src='".APP_PATH_IMAGES."application_view_icons.png' class='imgfix2'>Mass Delete a bunch of records");
-
-##### VALIDATION #####
-# Make sure user has permissions for project or is a super user
-$these_rights = REDCap::getUserRights(USERID);
-$my_rights = $these_rights[USERID];
-if (!$my_rights['design'] && !SUPER_USER) {
-	showError('Project Setup rights are required to access MASS DET plugin.');
-	exit;
-}
-# Make sure the user's rights have not expired for the project
-if ($my_rights['expiration'] != "" && $my_rights['expiration'] < TODAY) {
-	showError('Your user account has expired for this project.  Please contact the project admin.');
-	exit;
-}
-
-// TODO - get records by arm only...
-// Get all records (across all arms/events) and DAGs
-$record_data = REDCap::getData('array',NULL,REDCap::getRecordIdField(),NULL,NULL,FALSE,TRUE);
-$records = array_keys($record_data);
-
-// Are DAGs enabled (check for presence of 'redcap_data_access_group' on the first record)
-$first_record = reset($record_data);
-$first_event = reset($first_record);
-$dag_enabled = isset($first_event['redcap_data_access_group']);
-
-global $Proj;
-$records = array();
-
-// Get the current Instuments
-if ($Proj->longitudinal && $Proj->multiple_arms) {
-    // Multi-arm-mode
-
-    // Get record list per arm
-	$arms_records = Records::getRecordListPerArm();
-
-	// Get the current arm
-	$arm = isset($_POST['arm']) ? $_POST['arm'] : 1;
-
-	// Build select
-	$arms_options = array();
-	foreach ($Proj->events as $arm_num => $arm_detail) {
-		$arms_options[] = "<option value='$arm_num'" .
-			($arm_num == $arm ? " selected='selected'":"") . ">" .
-			$arm_detail['name'] .
-			(isset($arms_records[$arm_num]) ? " - " . count($arms_records[$arm_num]) . " records" : " - 0 records") .
-            "</option>";
-	}
-	$records = array_keys($arms_records[$arm]);
-} else {
-    // Non-multi-arm mode
-	$record_data = REDCap::getData('array',NULL,REDCap::getRecordIdField());
-	$records = array_keys($record_data);
-}
-
-$dags = REDCap::getGroupNames();
-if ( !empty($dags) ) {
-    // Get the dag of the curernt user
-    $user_rights = REDCap::getUserRights(USERID);
-    $this_group_id = $user_rights[USERID]['group_id'];
-    $this_group_id = 1;
-
-    $this_group_name = REDCap::getGroupNames(true, $this_group_id);
-    if ($this_group_id == '') {
-        // Not assigned - gets all records!
-	} else {
-		// User is in a DAG, so filter records
-        $dag_records = array();
-		$record_data = REDCap::getData('array',NULL,REDCap::getRecordIdField(),NULL,NULL,FALSE,TRUE);
-        foreach ($record_data as $record => $events) {
-            $first_event = reset($events);
-            $record_dag = $first_event['redcap_data_access_group'];
-            if ($record_dag == $this_group_name) {
-                $dag_records[] = $record;
-            }
-        }
-
-        $records = array_intersect($records, $dag_records);
-
-	}
-}
-
-print "<pre>DAGS" . print_r($dags,true) . "</pre>";
-
-
-// Handle Post
-$post_records = array();
-if (isset($_POST['Run'])) {
-
-	// DELETE THE RECORD
-	print "Posting!";
-
-	$arm_id = null;
-	if (isset($_POST['arm']) && $Proj->longitudinal && $Proj->multiple_arms) {
-		$arm_id = $Proj->getArmIdFromArmNum($_POST['arm']);
-		// Error: arm is incorrecct
-		if (!$arm_id) die("Invalid Arm ID from " . $_POST['arm']);
-
-		// Set event_id (for logging only) so that the logging denotes the correct arm
-		$_POST['event_id'] = $Proj->getFirstEventIdArm($_POST['arm']);
-	}
-
-	$post_records = $_POST['records'];
-	$record_list = array_intersect($post_records,$records);
-
-	//public static function deleteRecord($fetched, $table_pk, $multiple_arms, $randomization, $status,
-	//									$require_change_reason, $arm_id=null, $appendLoggingDescription="")
-
-	foreach ($record_list as $record) {
-	    $table_pk = REDCap::getRecordIdField();
-        print "<br>Deleting $record";
-
-	    Records::deleteRecord(
-            $record,
-            $Proj->table_pk,
-            $Proj->multiple_arms,
-            $Proj->project_id['randomization'],
-            $Proj->project['status'],
-            $Proj->project['require_change_reason'],
-            $arm_id,
-            " (" . $em->getModuleName() . ")"
-        );
-    }
-}
-
+$massDelete->validateUserRights('design');
+$massDelete->determineRecords();
+$massDelete->handlePost();
 
 
 // Render Page
-$cbx_array = array();
-$max_length = 0;
-foreach ($records as $record) {
-	$cbx_array[] = "<input type='checkbox' name='records[]' value='$record' " .
-		(in_array($record, $post_records) ? "checked" : "" ) .
-		">$record";
-	$max_length = max($max_length,strlen($record));
-}
-$max_length = $max_length + 3; // add space for checkbox
+renderPageTitle("<img src='".APP_PATH_IMAGES."application_view_icons.png' class='imgfix2'>&nbsp;Mass Delete a bunch of records");
 
+if (!empty($massDelete->errors)) {
+	print "<div class='alert alert-danger'><ul><li>" . implode("</li><li>", $massDelete->errors) . "</li></ul></div>";
+}
+
+if (!empty($massDelete->notes)) {
+    print "<div class='alert alert-success'><ul><li>" . implode("</li><li>", $massDelete->notes) . "</li></ul></div>";
+}
 
 ?>
-<h3>This plugin will delete a bunch of records!</h3>
-<form method='POST'>
-    <div>
-        <label for="arms">Select Arms: </label><select name='arms'><?php echo implode('',$arms_options) ?></select>
+<form class="delete_records" method='POST'>
+<!--<div class="container">-->
+    <!--<div class="row">-->
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4>Mark Records for Deletion</h4>
+					<?php if (!empty($massDelete->arm_options)) { ?>
+                        <div>
+                            <label for="arms">Arm:&nbsp;</label><select name="arm"><?php echo implode('',$massDelete->arm_options) ?></select>
+                        </div>
+					<?php } ?>
 
-
-        <?php print $arms_select ?>
-    </div>
-    <fieldset>
-        <legend>
-            Select Records to Delete
-            <span data-choice='all' class='sel jqbutton'/>All</span>
-            <span data-choice='none' class='sel jqbutton'/>None</span>
-            <span data-choice='custom' class='customList jqbutton'/>Custom List</span>
-        </legend>
-        <br/>
-        <div class="wrapper">
-            <ul><li><?php print implode("</li><li>",$cbx_array) ?></li></ul><br/>
-        </div>
-    </fieldset>
-    <br/>
-    <input type='submit' name='Run' class='jqbutton'/>
+                </div>
+                <div class="panel-body">
+                    <div class="wrapper">
+                        <ul><li><?php print implode("</li><li>",$massDelete->record_checkboxes) ?></li></ul>
+                    </div>
+                </div>
+                <div class="panel-footer">
+                    <span data-choice='all'    class="btn btn-default sel"/>All</span>
+                    <span data-choice='none'   class='btn btn-default sel'/>None</span>
+                    <span data-choice='custom' class='btn btn-default customList'/>Custom List</span>
+                    <span id="delete" data-choice='delete' class="btn btn-danger">Delete Selected Records</span>
+                </div>
+            </div>
+    <!--</div>   name="Run" value="Delete Selected Records" -->
+<!--</div>-->
 </form>
+
 <style>
-    button.sel {margin: 0px 10px; font-weight:bold; padding: 5px;}
-    legend {font-weight: bold; font-size:larger;}
-    fieldset {padding: 5px; max-width: 600px;}
-    input.url { width: 600px;}
-    form div {padding-bottom: 10px;}
-    div.det_results {background: rgb(247,248,249); color: #333; padding:10px;}
+    div.alert { border: inherit !important; }
     .wrapper {overflow:auto; max-height: 300px;}
-    /*	.wrapper ul {width: <?php echo floor(500/$max_length) ?>em;} */
-    .wrapper ul li {float:left; width: <?php echo $max_length ?>em; display:inline-block;}
+    .wrapper ul li {float:left; width: <?php echo $massDelete->max_length ?>em; display:inline-block;}
     .wrapper br {clear:left;}
     .wrapper {margin-bottom: 1em;}
     .cr {width: 100%; height: 200px; overflow:auto;}
+    .delete_btn { background-color: red; }
+    .delete_btn:hover { background-color: red; background-image: none; }
 </style>
 <script type='text/javascript'>
     $(document).ready( function() {
@@ -195,16 +70,54 @@ $max_length = $max_length + 3; // add space for checkbox
             return false;
         });
 
+        $('#delete').click( function() {
+            var num_selected = $('input[name="records[]"]:checked').length;
+            if (num_selected == 0) {
+                simpleDialog('<b>You must first at least one record</b>');
+                return false;
+            }
+
+            initDialog("confirmDeletion");
+            $('#confirmDeletion')
+                .html("Are you sure you want to delete the " + num_selected + " selected records!  This is permanent")
+                .dialog({
+                    title: "Confirm Deletion",
+                    bgiframe:true,
+                    modal:true,
+                    width:550,
+                    close: function() { $(this).dialog('destroy'); },
+                    open: function(){ fitDialog(this) },
+                    buttons: {
+                        'Cancel': function() { $(this).dialog('destroy'); },
+                        'Delete': function() {
+                            var input = $("<input>")
+                                .attr("type", "hidden")
+                                .attr("name", "delete").val("true");
+                            $('form.delete_records').append(input).submit();
+                        }
+                    },
+                    create:function () {
+                        var b = $(this).closest(".ui-dialog")
+                            .find(".ui-dialog-buttonset .ui-button:last").addClass("delete_btn");
+                    }
+                });
+
+            console.log (num_selected);
+            // var state = $(this).data('choice') == 'all';
+            // $('input[name="records[]"]').prop('checked',state);
+            return false;
+        });
+
+
         $('span.customList').click( function() {
-            console.log("Here!");
             // Open up a pop-up with a list
             var data = "<p>Enter a comma-separated or return-separated list of record ids to select</p><textarea class='cr' name='custom_records' placeholder='Enter a comma-separated list of record_ids'></textarea>";
             initDialog("custom_records_dialog", data);
             $('#custom_records_dialog').dialog({ bgiframe: true, title: 'Enter Custom Record List',
                 modal: true, width: 650,
                 buttons: {
-                    Close: function() {  },
-                    Apply: function() {
+                    'Close': function() {  },
+                    'Apply': function() {
                         // Parse out contents
                         var list = $('#custom_records_dialog textarea').val();
                         var items = $.map(list.split(/\n|,/), $.trim);
@@ -212,7 +125,6 @@ $max_length = $max_length + 3; // add space for checkbox
                             console.log (i, e);
                             $('input[value="' + e + '"]').prop('checked',true);
                         });
-//					console.log($('#custom_records_dialog textarea').val());
                         $(this).dialog('close');
                     }
                 }
@@ -220,31 +132,29 @@ $max_length = $max_length + 3; // add space for checkbox
         });
     });
 
-    function refreshArms() {
-        console.log(this);
-    }
+    $('select[name="arm"]').on('change',function() {
+        $(this).closest('form').submit();
+    });
+
 
 </script>
 
 
 
+<?php
 
 
 
-print "<pre>" . print_r($Proj,true) . "</pre>";
-
-
-print "<pre>" . print_r(REDCap::getInstrumentNames(),true) . "</pre>";
-
+// print "<pre>" . print_r($Proj,true) . "</pre>";
+// print "<pre>" . print_r(REDCap::getInstrumentNames(),true) . "</pre>";
 
 require_once \ExternalModules\ExternalModules::getProjectFooterPath();
 
-
-#display an error from scratch
-function showError($msg) {
-	$HtmlPage = new HtmlPage();
-	$HtmlPage->PrintHeaderExt();
-	echo "<div class='red'>$msg</div>";
-	//Display the project footer
-	require_once APP_PATH_DOCROOT . 'ProjectGeneral/footer.php';
-}
+// #display an error from scratch
+// function showError($msg) {
+// 	$HtmlPage = new HtmlPage();
+// 	$HtmlPage->PrintHeaderExt();
+// 	echo "<div class='red'>$msg</div>";
+// 	//Display the project footer
+// 	require_once APP_PATH_DOCROOT . 'ProjectGeneral/footer.php';
+// }
