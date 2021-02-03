1ebcd0299b6f897a169452f733fd5f7338e9f252
Ekin Tertemiz <ekin.tertemiz@swisstph.ch>
2021-02-03 16:29:27 +0100

    Add request handler.

--- requestHandler.php
+++ requestHandler.php
@@ -1,2 +1,9 @@
 <?php
 /** @var \ Stanford\MassDelete $module */
+
+if ($_REQUEST['type'] == 'triggerFetchRecords') {
+    $module->fetchRecords(
+        $_POST["arm_id"],
+        $_POST["group_id"]
+    );
+}
