6e86ba8cae6157db154aa2ab5cf0b8289fc98adb
Andy Martin <andy123@stanford.edu>
2022-10-27 08:31:29 -0700

    fixed taint report and removed what seemed to be an unneeded option around arms

--- page_mass_delete.php
+++ page_mass_delete.php
@@ -5,19 +5,23 @@ namespace Stanford\MassDelete;
 $module->init_page();
 
 global $Proj;
-$hasArms = $Proj->longitudinal && $Proj->multiple_arms; ?>
+$hasArms = $Proj->longitudinal && $Proj->multiple_arms;
+
+$view = isset($_GET['view']) ? htmlspecialchars($_GET['view'], ENT_QUOTES) : "na";
+
+?>
 
 <div class="container">
 
     <?php if( isset($_POST['result'])):  ?>
         <p><a href="<?= $module->getUrl("page_mass_delete.php") ?> "><button class="btn btn-primary"><i class="fas fa-arrow-circle-left"></i> Click here to go back.</button></a></p>
     <?php else : ?>
-        <div id="<?= $_GET['view'] ?>" class="row" >
+        <div id="<?= $view ?>" class="row" >
             <div class="col">
                 <form method="POST" class="delete_records">
                 <input type="hidden" name="result" value="delete" >
                 <input type="hidden" name="group_id" value="<?= $module->group_id ?>">
-                <input type="hidden" name="mode" value="<?= $_GET['view'] ?>">
+                <input type="hidden" name="mode" value="<?= $view ?>">
                 <?php if($hasArms): ?>
                     <div class="form-group">
                         <label>Project arm</label>
@@ -28,14 +32,13 @@ $hasArms = $Proj->longitudinal && $Proj->multiple_arms; ?>
                                     echo "<option value='$arm_num'> {$arm_detail['name']}";                
                                 }                            
                             ?>
-                            <?= implode('', $module->arm_options) ?>                    
                         </select>
                         <small id="selectHelpBlock" class="form-text text-muted">
                             This option has to be defined for longitudinal projects with multiple arms.
                         </small>
                     </div>
                 <?php endif; ?>                       
-                <?php if( isset($_GET['view']) && $_GET['view'] == 'custom-list' ): ?>
+                <?php if( $view == 'custom-list' ): ?>
                     <div class="form-group">
                         <div class="input-group">
                             <textarea <?= $hasArms ? "disabled" : "" ?> class="form-control list-input-step"  rows="10" aria-label="With textarea"></textarea>
@@ -53,7 +56,7 @@ $hasArms = $Proj->longitudinal && $Proj->multiple_arms; ?>
                     <div class="form-group">
                         <ul id="custom-output"></ul>
                     </div>
-                <?php elseif( isset($_GET['view']) && $_GET['view'] == 'record-list' ): ?>
+                <?php elseif( $view == 'record-list' ): ?>
                     <div id="fetchWrapper" class="form-group">
                         <p>Fetch all records for this project and select those which you would like to delete.</p>
                         <button <?= $hasArms ? 'disabled' : '' ?> id="btn-fetch-records" type="button" class="btn btn-primary list-input-step">
